05de1ee762af820badc0d41b8fdf8a3b0056f2ad
andikafrahenta <65215333+andikafrahenta@users.noreply.github.com>
2020-05-12 14:00:38 +0700

    Add files via upload

---
+++ chart/config.php
@@ -0,0 +1,14 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'covid';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$conn = new mysqli($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+if($conn -> connect_errno){
+	echo die ("Gagal Menghubungkan ke Databases".$conn->connect_error);
+}
+?>
+
+
+
